1933e931682909857e8554d6b387b3a441dc9094
oxtna <rafalkajomof2709@gmail.com>
2021-11-12 19:09:49 +0100

    Make navbar change color from transparent to black when scrolled

---
+++ js/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const nav = document.getElementsByClassName("nav")[0];
+
+window.addEventListener("scroll", () => {
+	if (window.scrollY > 0) {
+		nav.classList.add("scrolled");
+	} else {
+		nav.classList.remove("scrolled");
+	}
+});
