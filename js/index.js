c2bce86afa02a11d58660879f9ba579797ff9c28
oxtna <rafalkajomof2709@gmail.com>
2021-11-26 23:04:04 +0100

    Move projects related containers to css/projects.css and add card clicking behavior

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const nav = document.getElementsByClassName("nav")[0];
-
 window.addEventListener("scroll", () => {
 	if (window.scrollY > 0) {
 		nav.classList.add("scrolled");
@@ -9,3 +8,13 @@ window.addEventListener("scroll", () => {
 		nav.classList.remove("scrolled");
 	}
 });
+
+const cards = document.getElementsByClassName("card");
+Array.from(cards).forEach(card => card.addEventListener("click", () => {
+	if (card.classList.contains("clicked")) {
+		card.classList.remove("clicked");
+	}
+	else {
+		card.classList.add("clicked");
+	}
+}));
