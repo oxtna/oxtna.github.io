6e7f6c564cfff9a5fedb868a5d175cc0d0cf6f28
oxtna <rafalkajomof2709@gmail.com>
2021-12-05 14:50:49 +0100

    Add content fading in during scrolling

--- js/index.js
+++ js/index.js
@@ -30,3 +30,21 @@ terminalTabs.forEach((terminalTab) =>
 		}
 	})
 );
+
+const contentObserverOptions = {
+	root: null,
+	rootMargin: "0px 0px -10% 0px"
+};
+
+const contentObserver = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+		if (!entry.isIntersecting) {
+			return;
+		}
+		entry.target.classList.add("appear");
+		observer.unobserve(entry.target);
+	});
+}, contentObserverOptions);
+
+const contentWrappers = document.querySelectorAll(".fade-in");
+contentWrappers.forEach(contentWrapper => contentObserver.observe(contentWrapper));
