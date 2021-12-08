24550af75d215e281655746bc2909422e3766154
oxtna <rafalkajomof2709@gmail.com>
2021-12-08 02:33:52 +0100

    Clean up index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,15 @@
 
 const clickEvent = new Event("click");
 
+// HTML document's elements
 const nav = document.querySelector(".nav");
+const cards = document.querySelectorAll(".card");
+const buttons = document.querySelectorAll(".button");
+const terminalTabs = document.querySelectorAll(".terminal-tab");
+const terminalPrompt = document.querySelector(".terminal-prompt");
+const terminalOutputs = document.querySelectorAll(".terminal-output");
+const contentWrappers = document.querySelectorAll(".fade-in");
+
 window.addEventListener("scroll", () => {
 	if (window.scrollY > 0) {
 		nav.classList.add("scrolled");
@@ -11,22 +19,20 @@ window.addEventListener("scroll", () => {
 	}
 });
 
-const cards = document.querySelectorAll(".card");
 cards.forEach((card) =>
 	card.addEventListener("click", () => {
 		card.classList.toggle("reversed");
 	})
 );
 
-const terminalTabs = document.querySelectorAll(".terminal-tab");
-const terminalPrompt = document.querySelector(".terminal-prompt");
-const terminalOutputs = document.querySelectorAll(".terminal-output");
 terminalTabs.forEach((terminalTab, index) => {
 	terminalTab.addEventListener("click", () => {
 		if (window.innerWidth >= 768) {
 			if (!terminalTab.classList.contains("selected")) {
 				terminalTab.classList.add("selected");
 				terminalOutputs.item(index).classList.remove("hidden");
+
+				// Reset typing animation
 				terminalPrompt.classList.remove("typing");
 				void terminalPrompt.offsetWidth;
 				terminalPrompt.classList.add("typing");
@@ -41,12 +47,13 @@ terminalTabs.forEach((terminalTab, index) => {
 	})
 });
 
-const buttons = document.querySelectorAll(".button");
 buttons.forEach((button, index) => {
 	button.addEventListener("click", () => {
 		if (!terminalTabs.item(index).classList.contains("selected")) {
 			terminalTabs.item(index).classList.add("selected");
 			terminalOutputs.item(index).classList.remove("hidden");
+
+			// Reset typing animation
 			terminalPrompt.classList.remove("typing");
 			void terminalPrompt.offsetWidth;
 			terminalPrompt.classList.add("typing");
@@ -61,11 +68,11 @@ buttons.forEach((button, index) => {
 	});
 });
 
+// Make IntersectionObserver responsible for managing content that will fade in after scrolling down to it.
 const contentObserverOptions = {
 	root: null,
 	rootMargin: "0px 0px -10% 0px"
 };
-
 const contentObserver = new IntersectionObserver((entries, observer) => {
 	entries.forEach((entry) => {
 		if (!entry.isIntersecting) {
@@ -75,6 +82,4 @@ const contentObserver = new IntersectionObserver((entries, observer) => {
 		observer.unobserve(entry.target);
 	});
 }, contentObserverOptions);
-
-const contentWrappers = document.querySelectorAll(".fade-in");
 contentWrappers.forEach((contentWrapper) => contentObserver.observe(contentWrapper));
