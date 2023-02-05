dacd6321dc474ddc105f370c81d4f450724c1ddd
oxtna <rafalkajomof2709@gmail.com>
2023-02-05 22:16:38 +0100

    Refactor helper classes into data attributes

--- js/index.js
+++ js/index.js
@@ -1,94 +1,93 @@
 "use strict";
 
-const clickEvent = new Event("click");
-
-// HTML document's elements
 const nav = document.querySelector(".nav");
 const menuButton = document.querySelector(".menu-button");
-const cards = document.querySelectorAll(".card");
-const buttons = document.querySelectorAll(".button");
+const terminal = document.querySelector(".terminal");
+const terminalButtons = document.querySelectorAll(".button[data-index]");
 const terminalTabs = document.querySelectorAll(".terminal-tab");
-const terminalPrompt = document.querySelector(".terminal-prompt");
 const terminalOutputs = document.querySelectorAll(".terminal-output");
-const contentWrappers = document.querySelectorAll(".fade-in");
+const terminalPrompt = document.querySelector(".terminal-prompt");
+const fadingInElements = document.querySelectorAll(".fade-in");
 
 window.addEventListener("scroll", () => {
 	if (window.scrollY > 0) {
-		nav.classList.add("scrolled");
+		nav.dataset.scrolled = "true";
 	} else {
-		nav.classList.remove("scrolled");
+		nav.dataset.scrolled = "false";
 	}
 });
 
 menuButton.addEventListener("click", () => {
-	menuButton.classList.toggle("open");
+	if (menuButton.dataset.open === "false") {
+		menuButton.dataset.open = "true";
+	} else {
+		menuButton.dataset.open = "false";
+	}
 });
 
 menuButton.addEventListener("blur", () => {
-	menuButton.classList.remove("open");
+	menuButton.dataset.open = "false";
 });
 
-cards.forEach((card) =>
-	card.addEventListener("click", () => {
-		card.classList.toggle("reversed");
-	})
-);
+const selectTerminalTabAndOutput = (index) => {
+	for (const terminalTab of terminalTabs) {
+		if (terminalTab.dataset.index === index) {
+			terminalTab.dataset.selected = "true";
+		} else {
+			terminalTab.removeAttribute("data-selected");
+		}
+	}
 
-terminalTabs.forEach((terminalTab, index) => {
+	for (const terminalOutput of terminalOutputs) {
+		if (terminalOutput.dataset.index === index) {
+			// Reset output animation
+			terminalOutput.removeAttribute("data-selected");
+			void terminalOutput.offsetWidth;
+			terminalOutput.dataset.selected = "true";
+		} else {
+			terminalOutput.removeAttribute("data-selected");
+		}
+	}
+
+	// Reset typing animation
+	terminalPrompt.dataset.typing = "false";
+	void terminalPrompt.offsetWidth;
+	terminalPrompt.dataset.typing = "true";
+};
+
+for (const terminalTab of terminalTabs) {
 	terminalTab.addEventListener("click", () => {
-		if (window.innerWidth >= 768) {
-			if (!terminalTab.classList.contains("selected")) {
-				terminalTab.classList.add("selected");
-				terminalOutputs.item(index).classList.remove("hidden");
-
-				// Reset typing animation
-				terminalPrompt.classList.remove("typing");
-				void terminalPrompt.offsetWidth;
-				terminalPrompt.classList.add("typing");
-			}
-			terminalTabs.forEach((otherTerminalTab, otherIndex) => {
-				if (otherTerminalTab !== terminalTab) {
-					otherTerminalTab.classList.remove("selected");
-					terminalOutputs.item(otherIndex).classList.add("hidden");
-				}
-			});
+		if (window.innerWidth <= 768) {
+			return;
 		}
-	})
-});
 
-buttons.forEach((button, index) => {
-	button.addEventListener("click", () => {
-		if (!terminalTabs.item(index).classList.contains("selected")) {
-			terminalTabs.item(index).classList.add("selected");
-			terminalOutputs.item(index).classList.remove("hidden");
-
-			// Reset typing animation
-			terminalPrompt.classList.remove("typing");
-			void terminalPrompt.offsetWidth;
-			terminalPrompt.classList.add("typing");
-		}
-		terminalTabs.forEach((otherTerminalTab, tabIndex) => {
-			if (index === tabIndex) {
-				return;
-			}
-			otherTerminalTab.classList.remove("selected");
-			terminalOutputs.item(tabIndex).classList.add("hidden");
-		})
+		selectTerminalTabAndOutput(terminalTab.dataset.index);
 	});
-});
+}
+
+for (const terminalButton of terminalButtons) {
+	terminalButton.addEventListener("click", () => {
+		selectTerminalTabAndOutput(terminalButton.dataset.index);
+	});
+}
 
 // Make IntersectionObserver responsible for managing content that will fade in after scrolling down to it.
-const contentObserverOptions = {
+const observerOptions = {
 	root: null,
 	rootMargin: "0px 0px -20% 0px"
 };
-const contentObserver = new IntersectionObserver((entries, observer) => {
+
+const observer = new IntersectionObserver((entries, observer) => {
 	entries.forEach((entry) => {
 		if (!entry.isIntersecting) {
 			return;
 		}
-		entry.target.classList.add("appear");
+
+		entry.target.dataset.appear = "true";
 		observer.unobserve(entry.target);
 	});
-}, contentObserverOptions);
-contentWrappers.forEach((contentWrapper) => contentObserver.observe(contentWrapper));
+}, observerOptions);
+
+for (const fadingInElement of fadingInElements) {
+	observer.observe(fadingInElement);
+}
